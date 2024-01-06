7f3d34459e47bdf46ec3818f515828fcb034a36f
Mariliis Toon <mariliistoon@gmail.com>
2024-01-06 13:03:47 +0200

    hindeline

--- Hindeline/Program.cs
+++ Hindeline/Program.cs
@@ -7,9 +7,20 @@
 //}
 
 
+//string userPIN;
+//int triesUsed = 0;
+//while (triesUsed !=  3) 
+//{
+//    Console.WriteLine("Enter your PIN");
+//    userPIN = Console.ReadLine();
+//    if ( userPIN == "1234")
+//    {
+//        Console.WriteLine("Welcome!");
+//        triesUsed = 3; //number
+//    }else
 
-
-for (int i = 1; i < 5; i++)
-{
-    Console.WriteLine("Don't worry, be happy :)");
-}
+//    {
+//        Console.WriteLine($"Wrong PIN. Try again. {3 - triesUsed} tries left.");
+//        triesUsed ++;
+//    }
+//}
